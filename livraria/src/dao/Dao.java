2db9c59b9d31a3901617b41e49d536ba11591236
Dyego Hitalo <dyego.hit@gmail.com>
2018-01-04 22:07:22 -0200

    Deletando projetos antigos.

--- livraria/src/dao/Dao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-import model.Livro;
-public interface Dao<ModelType> {
-    //CRUD = Criar , Recuperar, Alterar e Delete.
-    ModelType criar (ModelType modelo);
-    ModelType recuperar (Long id);
-    ModelType alterar (ModelType modelo);
-    ModelType delete (Long id);
-}
